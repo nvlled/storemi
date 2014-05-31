1d4844b8b9cf2003f9377ef3a51c89fdf4baa559
ron <nvlled@gmail.com>
2014-05-31 15:45:12 +0800

    Switch to jetty adapter

--- src/storemi/core.clj
+++ src/storemi/core.clj
@@ -2,14 +2,16 @@
   (:gen-class)
   (:use [ring.middleware file-info file])
   (:require 
+    [ring.adapter.jetty :as ring]
     [ring.server.standalone :refer [serve]]
     [storemi.handler :refer [app init]]))
 
 (defn -main [& [port]]
-  (serve 
-    (-> app
-        (wrap-file "resources")
-        wrap-file-info)
-    {:port (or (java.lang.Integer. port) 5050)
-     :init init
-     :join false}))
+  (let [port (if port (Integer. port) 6060)]
+    (ring/run-jetty 
+      (-> app
+          (wrap-file "resources")
+          wrap-file-info)
+      {:port port
+       :init init
+       :join false})))
