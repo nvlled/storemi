89f563332ddf730bdb3dd1f5e58328d1bcf74517
ron <nvlled@gmail.com>
2014-06-05 19:52:50 +0800

    Handle queries for browsing stories

--- src/storemi/routes/browse.clj
+++ src/storemi/routes/browse.clj
@@ -1,25 +1,43 @@
 (ns storemi.routes.browse
-  (:require 
+  (:require
     [compojure.core :refer :all]
     [storemi.lib.tourl :refer [retour]]
     [storemi.views.layout :as layout]
     [storemi.views.page :as page]
     [storemi.models.user :as user]
+    [storemi.models.story :as st]
+    [storemi.common :refer [read-int]]
     [storemi.session :as session]
     [storemi.urlfor :as urlfor]
-    [storemi.lib.rule :refer 
+    [storemi.lib.rule :refer
      [one-by-one
       has-params
       enforce]]))
 
-(defn match-query [request]
-  )
+(def stories-per-page 5)
+
+(defn make-page-data [page-number]
+  (let [start (* page-number stories-per-page)]
+    {:start start
+     :end (+ start stories-per-page)
+     :pagenum page-number
+     :size stories-per-page}))
+
+(defn match-query [{params :params :as request}]
+  (let [{:keys [query by page]} params
+        exact (= by "username")
+        page-data (make-page-data (read-int page 0))
+        stories
+        (st/query-stories
+          query
+          :criteria by
+          :exact exact
+          :page-data page-data)
+        ]
+    (page/browse request stories page-data)))
 
 (defroutes the-routes
-  (POST (urlfor/browse) _
-        match-query)
-  (GET (urlfor/browse) _
-       page/browse))
+  (GET urlfor/browse-path  -> match-query))
 
 
 
